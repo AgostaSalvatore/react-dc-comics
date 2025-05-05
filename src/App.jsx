314f4a9b141890f4ad0c2ae6f5577aa892ee6e01
AgostaSalvatore <agosta.boolean@gmail.com>
2025-05-05 16:19:31 +0200

    [UPD] main links (shop, digital comics ecc)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Header from "./components/Header"
 import MainContent from "./components/MainContent"
+import MainLinks from "./components/MainLinks"
 
 function App() {
   return (
     <>
     <Header/>
     <MainContent/>
-    
+    <MainLinks/>
     </>
   )
 }
